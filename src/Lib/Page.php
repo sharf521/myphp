d41e78eb7c16641b1f175734702f19f3e7b98d2a
sharf <373455742@qq.com>
2018-03-06 14:25:41 +0800

    page

--- src/Lib/Page.php
+++ src/Lib/Page.php
@@ -28,8 +28,8 @@ class Page
         }
         $multipage = '';
         if ($num > $perpage) {
-            $page = 10;
-            $offset = 5;
+            $page = 7;
+            $offset = 3;
             $pages = ceil($num / $perpage);
             if ($page > $pages) {
                 $from = 1;
@@ -51,14 +51,13 @@ class Page
                     }
                 }
             }
-            $multipage = ($curpage - $offset > 1 && $pages > $page ? '<a href="' . $mpurl . 'page=1" class="p_redirect">&laquo;</a>' : '') . ($curpage > 1 ? '<a href="' . $mpurl . 'page=' . ($curpage - 1) . '" class="p_redirect">&#8249;</a>' : '');
+            $multipage = ($curpage - $offset > 1 && $pages > $page ? '<a href="' . $mpurl . 'page=1" class="p_redirect" title="首页">&laquo;</a>' : '') . ($curpage > 1 ? '<a href="' . $mpurl . 'page=' . ($curpage - 1) . '" class="p_redirect" title="上一页">&#8249;</a>' : '');
             for ($i = $from; $i <= $to; $i++) {
                 $multipage .= $i == $curpage ? '<span class="p_curpage">' . $i . '</span>' : '<a href="' . $mpurl . 'page=' . $i . '" class="p_num">' . $i . '</a>';
             }
-            $multipage .= ($curpage < $pages ? '<a href="' . $mpurl . 'page=' . ($curpage + 1) . '" class="p_redirect">&#8250;</a>' : '') . ($to < $pages ? '<a href="' . $mpurl . 'page=' . $pages . '" class="p_redirect">&raquo;</a>' : '');
-            $multipage = $multipage ? '<div class="p_bar"><span class="p_info">总记录：' . $num . '</span>' . $multipage . '</div>' : '';
+            $multipage .= ($curpage < $pages ? '<a href="' . $mpurl . 'page=' . ($curpage + 1) . '" class="p_redirect" title="下一页">&#8250;</a>' : '') . ($to < $pages ? '<a href="' . $mpurl . 'page=' . $pages . '" class="p_redirect" title="尾页">&raquo;</a>' : '');
+            $multipage = $multipage ? '<div class="p_bar"><span class="p_info">总 ' . $num . ' 条</span>' . $multipage . '</div>' : '';
         }
         return $multipage;
     }
 }
-
